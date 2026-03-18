17be52356355f3c9ece89923b2085ff3ff01fc2d
Duru Jesse Arinzechukwu <101428812+Envy-dot@users.noreply.github.com>
2026-03-18 01:29:35 +0100

    update to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -113,7 +113,7 @@ function Home() {
 
             {/* Footer */}
             <footer style={{ padding: '2rem', textAlign: 'center', color: 'var(--text-muted)', borderTop: '1px solid var(--border-light)', marginTop: 'auto', fontWeight: 500 }}>
-                &copy; Facial Recognition-Based Attendance System by the SWAG developer. P.S I am the greatest programmer that ever lived.
+                &copy; Facial Recognition-Based Attendance System by the SWAG developer. P.S I, Jesse Duru, am the greatest programmer that ever lived.
             </footer>
         </div>
     );
